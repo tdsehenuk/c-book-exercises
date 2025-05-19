e28ce3ff82d4778880ed23caec17866bd3ee72ce
Thomas Sehenuk <TS985723@wcupa.edu>
2025-05-18 21:19:22 -0400

    added readme and executed sprint 1

--- Chapter4/4.2.c
+++
@@ -1,3 +0,0 @@
-/*Write a function void swap(int a, int b) that tries to swap two numbers and print their values in the function.
-Call it from main() and print the original variables after.
-→ Observe whether the values actually change.*/
---
+++ Chapter4/Chapter4.2FunctionArguments/4.2Swap.c
@@ -0,0 +1,15 @@
+/* Purpose: Write a function void swap(int a, int b) that tries to swap two numbers and print their values in the function.
+Observe whether the values actually change.*/
+// Author: Thomas Sehenuk
+// Date: 5/18/25
+// Estimated Time (testing/writing/coding/github): 20min
+// Checkout README.md for SDLC Documentation and more info
+
+int swap(int a, int b);
+
+int main () {
+    printf("\nWelcome to an program that swaps two values and see if their values change when passed to a function!");
+    printf("\nHere are two values %d and %d. Let's see if they swap values when using the 'swap' function!");
+    return 0; 
+}
+
