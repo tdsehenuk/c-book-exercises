358005c5cc1906b3676cd5bb4f2c3be247bd7c34
Thomas Sehenuk <TS985723@wcupa.edu>
2025-05-18 21:30:38 -0400

    sprint 2 done

--- Chapter4/Chapter4.2FunctionArguments/4.2Swap.c
+++ Chapter4/Chapter4.2FunctionArguments/4.2Swap.c
@@ -5,11 +5,19 @@ Observe whether the values actually change.*/
 // Estimated Time (testing/writing/coding/github): 20min
 // Checkout README.md for SDLC Documentation and more info
 
-int swap(int a, int b);
+#include <stdio.h>
+
+void swap(int a, int b);
 
 int main () {
+    int old1 = 10;
+    int old2 = 20;
     printf("\nWelcome to an program that swaps two values and see if their values change when passed to a function!");
-    printf("\nHere are two values %d and %d. Let's see if they swap values when using the 'swap' function!");
+    swap(old1, old2);
     return 0; 
 }
 
+void swap(int a, int b) {
+    printf("\nHere are two values %d and %d. Let's see if they swap values when using the 'swap' function!", a, b);
+ 
+}
