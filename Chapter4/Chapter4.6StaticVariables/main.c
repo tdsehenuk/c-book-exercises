4794df531bf50a6ec690a3c2828def7444a13e73
Thomas Sehenuk <TS985723@wcupa.edu>
2025-05-19 20:20:03 -0400

    sprint 3

--- Chapter4/Chapter4.6StaticVariables/main.c
+++ Chapter4/Chapter4.6StaticVariables/main.c
@@ -5,17 +5,21 @@ Call it 5 times in a loop from main().
 
 // Author: Thomas Sehenuk
 // Date: 5/18/25
-// Estimated Time (testing/writing/coding/github): 15min
+// Estimated Time (testing/writing/coding/github): 13min
 // Checkout README.md for SDLC Documentation and more info
+
+#include <stdio.h>
 
 void counter() {
     int static count = 0;
     count++;
-    printf("count is equal to: %d ", count);
+    printf("\ncount is equal to: %d ", count);
 } 
 
 int main() {
-
+    for(int i = 0; i < 5; i++) {
+        counter();
+    }
 
     return 0;
 }
