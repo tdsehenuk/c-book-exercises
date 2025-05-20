83166f2063695053a20581eab361e4b45f84d16d
Thomas Sehenuk <TS985723@wcupa.edu>
2025-05-19 20:47:29 -0400

    sprint 2/3

--- Chapter4/Chapter4.9Initalization/main.c
+++ Chapter4/Chapter4.9Initalization/main.c
@@ -10,12 +10,24 @@ Print both before and after a loop that runs 3 times and modifies them.
 
 #include <stdio.h> 
 
-
+void test() {
+    static int num = 5;
+    int temp = 5;
+    printf("\nThe value of num = %d", num);
+    printf("\nThe value of temp = %d", temp);
+    num++;
+    temp++;
+}
 
 int main() {
 
 
 
     printf("\nWelcome to my program where i will be showing initialization!");
+
+    for (int i = 0; i<3; i++) {
+        test();
+    }
+
     return 0;
 }
