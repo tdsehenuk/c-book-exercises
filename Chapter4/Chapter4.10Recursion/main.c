8233c8007daf167979d715fb87be1e7860252914
Thomas Sehenuk <TS985723@wcupa.edu>
2025-05-19 21:00:59 -0400

    sprint 2

--- Chapter4/Chapter4.10Recursion/main.c
+++ Chapter4/Chapter4.10Recursion/main.c
@@ -9,6 +9,15 @@ Test it with factorial(5).*/
 
 #include <stdio.h>
 
+int factorial(int n) {
+    if(n <= 1) {
+        return 0;
+    } else {
+        return n*factorial(n-1);
+    }
+
+}
+
 int main() {
 
     printf("\nWelcome to my app where i will be showing recursion!");
