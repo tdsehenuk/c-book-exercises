2552244325a9243f5858920d3bdb20cc86809ef0
Thomas Sehenuk <TS985723@wcupa.edu>
2025-05-19 20:23:46 -0400

    sprint 1

--- Chapter4/Chapter4.7RegisterVariables/main.c
+++ Chapter4/Chapter4.7RegisterVariables/main.c
@@ -1,9 +1,21 @@
 /*Problem 7 (optional):
 Write a function that uses register int i in a loop and explain why the output is no different from a regular int.
-
-(This is mostly historical — modern compilers ignore it.)*/
+*/
 
 // Author: Thomas Sehenuk
 // Date: 5/18/25
 // Estimated Time (testing/writing/coding/github): 15min
 // Checkout README.md for SDLC Documentation and more info
+
+#include <stdio.h>
+#include <stdlib.h> 
+
+void fun() {
+    register int i; 
+}
+
+int main() {
+    
+
+    return 0;
+}
