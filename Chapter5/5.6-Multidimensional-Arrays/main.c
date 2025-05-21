26f7ec1c2e6d2cad815a32461906ffe251a67d13
Thomas Sehenuk <TS985723@wcupa.edu>
2025-05-20 23:06:57 -0400

    Sprint 3: Print 2D Array

--- Chapter5/5.6-Multidimensional-Arrays/main.c
+++ Chapter5/5.6-Multidimensional-Arrays/main.c
@@ -20,10 +20,20 @@ Print the modified matrix to confirm the changes.
 
 int main () {
     
-    int a[3][3] = 
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9};
+    int a[3][3] = { {1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 9}
+    };
+
+    int *pa = (int *)a;
+
+    for(int i = 0; i < 3; i++) {
+        for (int j = 0; j<3; j++) {
+           
+            printf("\n [%d]", *(pa+i * 3+j) );
+        }
+    }
+        
 
     return 0;
 }
