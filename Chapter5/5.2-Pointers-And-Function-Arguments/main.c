fb34d0825674c74d49e877d729240751baa03507
Thomas Sehenuk <TS985723@wcupa.edu>
2025-05-20 21:12:01 -0400

    Sprint 1: Skeleton and Stub

---
+++ Chapter5/5.2-Pointers-And-Function-Arguments/main.c
@@ -0,0 +1,21 @@
+/*
+Write a function void swap(int *a, int *b) that swaps the values of two integers using pointers.
+In main(), declare two integers, initialize them (e.g., int x = 10, y = 20;), and call swap(&x, &y).
+Print the values before and after the function call to verify that the values have been swapped using pointers.
+*/
+
+//SLDC
+// Author: Thomas Sehenuk
+// Date: 5/20/25
+// Estimated Time (testing/writing/coding/github): 20min
+// Time with Extended Problem in Step 6: + 10min
+// Checkout README.md for SDLC Documentation and more info
+
+#include <stdio.h>
+void swap(int *x, int *y); 
+
+int main () {
+
+
+    return 0;
+}
