3c3af86e51f1f774b0afb16fa2ceac4ceb537def
Thomas Sehenuk <TS985723@wcupa.edu>
2025-05-21 23:43:25 -0400

    Sprint 1: Skeleton and Student Struct

--- Chapter6/6.3/arrays_of_structures.c
+++ Chapter6/6.3/arrays_of_structures.c
@@ -1,8 +1,27 @@
-// arrays of structures 
+// accessing structure members 
 // Author: Thomas Sehenuk
 // Date: 5/21/25
+//Time Start: 11:41
+//Time Done: 11:
+
+/*
+Write a program that initializes a Student structure with values and prints 
+the details of the student. Use a function to print the details.
+
+Define a structure Student that contains:
+A string for the student's name.
+An integer for the student's age.
+A float for the student's GPA.
+*/
 
 #include <stdio.h>
+
+struct Student {
+    char name[25];
+    int age;
+    float gpa;
+};
+
 
 int main() {
     // TODO: Implement arrays of structures
