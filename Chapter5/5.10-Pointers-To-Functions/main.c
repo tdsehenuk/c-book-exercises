d3fcc4359456e27418b99f3eff528bfa2d00c1a8
Thomas Sehenuk <TS985723@wcupa.edu>
2025-05-21 20:24:11 -0400

    Sprint 1: Defining Add and Subtract

---
+++ Chapter5/5.10-Pointers-To-Functions/main.c
@@ -0,0 +1,35 @@
+/*
+Write a program that:
+Declares and initializes two functions, one that performs an addition (add) and another that performs a subtraction (subtract).
+Declares a pointer to a function and uses this pointer to call both functions.
+Demonstrates the use of function pointers by switching the pointer between the two functions and calling them.
+You are required to:
+Use a function pointer to call the add and subtract functions dynamically.
+Show how the function pointer can be reassigned to point to different functions during runtime.
+*/
+
+//SLDC
+// Author: Thomas Sehenuk
+// Date: 5/21/25
+// Estimated Time (testing/writing/coding/github): 25min
+// Time with Extended Problem in Step 6: + 0min
+// Checkout README.md for SDLC Documentation and more info
+
+#include <stdio.h>
+
+int add(int a, int b);
+int subtract(int a, int b);
+
+int main() {
+    printf("\nAdding two numbers 1 + 3 = %d", add(1,3));
+    printf("\nAdding two numbers 1 - 3 = %d", subtract(1,3));
+}
+
+int add(int a, int b) {
+    return a+b;
+}
+    
+
+int subtract(int a, int b) {
+    return a-b;
+}
