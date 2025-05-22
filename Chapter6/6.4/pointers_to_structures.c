7a7581da5de69b44969c0ccd5921315623ad2709
Thomas Sehenuk <TS985723@wcupa.edu>
2025-05-22 00:09:35 -0400

    Sprint 1: Skeleton + Car Structure

--- Chapter6/6.4/pointers_to_structures.c
+++ Chapter6/6.4/pointers_to_structures.c
@@ -1,10 +1,19 @@
-// pointers to structures 
+// accessing structure members 
 // Author: Thomas Sehenuk
-// Date: 5/21/25
+// Date: 5/22/25
+//Time Start: 12:08am
+//Time Done: 12:
 
 #include <stdio.h>
+
+struct Car {
+    char model[30];
+    int year;
+    float price;
+};
 
 int main() {
     // TODO: Implement pointers to structures
     return 0;
 }
+
