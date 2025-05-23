e7939a429cfe6b6e1ca48b7727e7330ce5f96ed1
Thomas Sehenuk <TS985723@wcupa.edu>
2025-05-22 23:28:06 -0400

    Learning Linked List?

--- Chapter6/6.5/self_referential_structures.c
+++ Chapter6/6.5/self_referential_structures.c
@@ -1,10 +1,46 @@
 // self referential structures 
 // Author: Thomas Sehenuk
-// Date: 5/21/25
+// Date: 5/22/25
+//Time Start: 10:44pm 
+//Time Stop: 
 
 #include <stdio.h>
+#include <stdlib.h> 
+
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+
+void insert_first(Node **head, int value) {
+    Node *n1 = (Node *)malloc(sizeof(Node));
+    if(n1 == NULL) {
+        printf("failed memory allocation");
+    }
+
+    n1->data = value;
+    n1->next = *head;
+    *head = n1;
+}
+
+void print_list(Node *head) {
+    Node *current = head; 
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+}
 
 int main() {
-    // TODO: Implement self referential structures
+    Node *head = NULL;
+    insert_first(&head, 10);
+    insert_first(&head, 20); 
+    insert_first(&head, 30);
+
+
+    print_list(head);
+
+
     return 0;
 }
