8ac0ffb584090b06570322a7b2d16229e62045d2
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-24 19:11:23 -0400

    Sprint 1: Skeleton Class

---
+++ Chapter7/7.1/stdio.c
@@ -0,0 +1,17 @@
+/*
+
+Write a program that reads characters one by one using getchar() and echoes them to the screen using putchar(), but:
+Replaces every space ' ' with an underscore '_'
+Converts lowercase letters to uppercase
+*/
+
+//Time Start 7:10
+//Time Stop 7:
+
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
