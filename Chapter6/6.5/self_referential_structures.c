f6eeaf973ea63cb7c1b9d190ff6947db8807b5cb
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-24 14:09:32 -0400

    Sprint 1: make a node struct + skeleton

--- Chapter6/6.5/self_referential_structures.c
+++ Chapter6/6.5/self_referential_structures.c
@@ -1,48 +1,14 @@
-// self referential structures 
-// Author: Thomas Sehenuk
-// Date: 5/22/25
-//Time Start: 10:44pm 
-//Time Stop: 
+//time start 2:06
+//time stop 
 
-#include <stdio.h>
-#include <stdlib.h> 
 
-typedef struct Node {
+
+typedef struct node {
     int data;
-    struct Node *next;
+    struct node *next;
 } Node;
 
-
-void insert_first(Node **head, int value) {
-    Node *n1 = (Node *)malloc(sizeof(Node));
-    if(n1 == NULL) {
-        printf("failed memory allocation");
-    }
-
-    n1->data = value;
-    n1->next = *head;
-    *head = n1;
-}
-
-void print_list(Node *head) {
-    Node *current = head; 
-    while (current != NULL) {git 
-        printf("%d -> ", current->data);
-        current = current->next;
-    }
-}
-
-
-
 int main() {
-    Node *head = NULL;
-    insert_first(&head, 10);
-    insert_first(&head, 20); 
-    insert_first(&head, 30);
 
-
-    print_list(head);
-
-
-    return 0;
+    return 0; 
 }
