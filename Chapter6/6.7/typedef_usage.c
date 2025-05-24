b3f5f69368ad9dfa73ab9c5e7c754e7d5f0b87e0
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-24 17:11:22 -0400

    Sprint 1: Showcasing typedef

--- Chapter6/6.7/typedef_usage.c
+++ Chapter6/6.7/typedef_usage.c
@@ -1,10 +1,27 @@
 // typedef usage 
 // Author: Thomas Sehenuk
 // Date: 5/21/25
+//Time start 5:05 
+//Time done: 5:10
+
 
 #include <stdio.h>
 
+typedef struct {
+    int x;
+    int y;
+}Point;
+
+void printPoint( Point p1) {
+    printf("\n(%d, %d)", p1.x, p1.y);
+}
+
 int main() {
-    // TODO: Implement typedef usage
+
+    Point p1; 
+    p1.x = 5;
+    p1.y = 10;
+    printPoint(p1);
+
     return 0;
 }
