f78306174c0e193060bb0eacf1d2fde8bea8aa56
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-24 19:36:34 -0400

    Sprint 1: Skeleton

---
+++ Chapter7/7.3/charactercounter.c
@@ -0,0 +1,21 @@
+/*
+Write a program that reads input (using getchar()) and counts how many characters of each type it sees:
+Digits (0-9)
+Whitespace characters (space, tab, newline)
+Everything else (punctuation, letters, symbols, etc.)
+Print the counts when EOF is reached
+*/
+
+
+//
+//time start 7:35
+//time stop
+
+
+int main() {
+
+
+
+
+    return 0;
+}
