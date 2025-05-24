56077061c933e64c3a76a614c5eb898db50424e8
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-24 19:54:32 -0400

    Sprint 1: Skeleton

---
+++ Chapter7/7.4/charTestingCovernsion.c
@@ -0,0 +1,20 @@
+//time start 7:53
+// time stop 7:
+
+/*
+Write a program that:
+Counts the number of:
+lowercase letters
+uppercase letters
+non-alphabetic characters
+
+*/
+
+#include <stdio.h>
+
+
+int main() {
+
+
+    return 0;
+}
