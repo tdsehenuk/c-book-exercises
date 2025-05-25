0aa21f800fcd5257e8b73b7564d0a9c6c05ee62b
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-25 00:45:31 -0400

    Sprint 1: Skeleton

---
+++ Chapter8/8.6/listDir.c
@@ -0,0 +1,13 @@
+/*
+Write a program that opens a directory, reads its contents, and prints out the file/directory names inside.
+*/
+
+//Time start
+//Time close
+
+int main () {
+
+
+    
+    return 0;
+}
