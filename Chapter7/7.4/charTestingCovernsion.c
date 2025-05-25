690c50403b2f3c1b34ac91e385e280fa7da62e6a
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-24 20:03:26 -0400

    Sprint 2: Printing+ Logic

--- Chapter7/7.4/charTestingCovernsion.c
+++ Chapter7/7.4/charTestingCovernsion.c
@@ -11,9 +11,28 @@ non-alphabetic characters
 */
 
 #include <stdio.h>
-
+#include <ctype.h>
 
 int main() {
+
+    int lowerc = 0;
+    int upperc = 0;
+    int notalpha = 0;
+    int c = 0;
+
+    while ( (c = getchar()) != EOF) {
+        if(islower(c)) {
+            lowerc++;
+        } else if(isupper(c))  {
+            upperc++;
+        } else {
+            notalpha++;
+        }
+    }
+
+    printf("\nLower Chars: %d", lowerc);
+    printf("\nUpper Chars: %d", upperc);
+    printf("\nNon Alphabetic Chars: %d", notalpha);
 
 
     return 0;
