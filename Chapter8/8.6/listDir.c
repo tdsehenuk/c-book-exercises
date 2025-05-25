9f6eeb6beeb7c1a75825fa20e53a327e6a949e38
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-25 00:46:58 -0400

    Sprint 2: Make directory

--- Chapter8/8.6/listDir.c
+++ Chapter8/8.6/listDir.c
@@ -5,9 +5,22 @@ Write a program that opens a directory, reads its contents, and prints out the f
 //Time start
 //Time close
 
-int main () {
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
 
+int main (int argc, int *argv[]) {
+    const char *dirname = ".";
+  
+    if (argc > 1) {
+        dirname = argv[1];  // Use directory name from command line if provided
+    }
 
-    
+    DIR *dir = opendir(dirname);
+    if (dir == NULL) {
+        perror("opendir");
+        return 1;
+    }
+
     return 0;
 }
