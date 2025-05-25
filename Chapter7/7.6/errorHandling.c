0bfc71f2e802307e17c6ad95ae2c1563b41b4ad1
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-24 20:29:15 -0400

    Sprint 2: File Pointers and Copying

--- Chapter7/7.6/errorHandling.c
+++ Chapter7/7.6/errorHandling.c
@@ -2,17 +2,11 @@
 Write a program that:
 
 Opens a file called "input.txt" for reading.
-
 Opens a file called "output.txt" for writing.
-
 Copies the contents from input to output character by character.
-
 After copying, checks if:
-
 End-of-file was reached naturally.
-
 Any I/O error occurred.
-
 Reports the status clearly using feof(), ferror(), and perror().
 */
 
@@ -22,6 +16,13 @@ Reports the status clearly using feof(), ferror(), and perror().
 #include <stdio.h>
 
 int main () {
+    FILE *input = fopen("input.txt", "r");   
+    FILE *output = fopen("output.txt", "w");
+
+    int c = 0;
+    while ((c = getc(input)) != EOF) {
+        putc(c, output);
+    }
 
     return 0;
 }
