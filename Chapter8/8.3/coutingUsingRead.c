334b594fe2d75c83cc31651ae6b3a36ec18e092e
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-24 23:36:17 -0400

    Sprint 2: Counting Tabs and NewLines

--- Chapter8/8.3/coutingUsingRead.c
+++ Chapter8/8.3/coutingUsingRead.c
@@ -19,5 +19,23 @@ int main(int argc, char *argv[]) {
     int inputFD = open(argv[1], O_RDONLY);
     int ouputFD = open(argv[2], O_RDONLY);
 
+    char buf[512]; 
+    int n;
+    int tabcounter;
+    int nlcounter;
+
+    while((n=read(inputFD, buf, sizeof(buf))) > 0) {
+        for(int i = 0; i < n; i++) {
+            if(buf[i] == '\n') nlcounter++;
+            if(buf[i] == '\t') tabcounter++;
+        }
+    }
+
+    if (n < 0) {
+        perror("Error reading file");
+        close(inputFD);
+        exit(1);
+    }
+
     return 0;
 }
