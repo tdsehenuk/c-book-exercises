b396772ae3ac12f30862195a7f8d44b10fa162dc
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-24 20:39:18 -0400

    Sprint 3: Added Error Logic for File I/O

--- Chapter7/7.6/errorHandling.c
+++ Chapter7/7.6/errorHandling.c
@@ -11,7 +11,7 @@ Reports the status clearly using feof(), ferror(), and perror().
 */
 
 //time start: 8:24
-//time stop 8:
+//time stop 8:39
 
 #include <stdio.h>
 
@@ -19,15 +19,36 @@ int main () {
     FILE *input = fopen("input.txt", "r");   
     FILE *output = fopen("output.txt", "w");
 
-    int c = 0;
-    while ((c = getc(input)) != EOF) {
-        putc(c, output);
-    
-    
-    fclose(input);
-    fclose(output);
+    if(input == NULL) {
+        perror("Error opening input");
+        return 1;
     }
 
+    if (output == NULL) {
+        perror("Error opening output");
+        fclose(input);
+        return 1;
+    }
+
+
+    int c = 0;
+    while ((c = getc(input)) != EOF) {
+        if(putc(c,output) == EOF) {
+            perror("Error writing to output");
+            fclose(input);
+            fclose(output);
+            putc(c, output);
+       }
+    }
+    
+    if(feof(input)) {
+        printf("Copy completed successfully!\n");
+    } else if(ferror(input)) {
+        perror("error reading input.txt");
+    }
+
+    fclose(input);
+    fclose(output);
 
 
     return 0;
