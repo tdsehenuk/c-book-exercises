5ac482ff9902ef272ef0d95a0d9166633b99c29d
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-24 21:47:13 -0400

    Sprint 1: Skeleton

---
+++ Chapter8/8.1/fileDescriptors.c
@@ -0,0 +1,18 @@
+/*
+Write a program that:
+Opens a file input.txt
+Writes the first 100 bytes into output.txt
+Use only open(), read(), write(), close()
+*/
+
+//time start 9:46
+//time stop: 
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    return 0;
+}
