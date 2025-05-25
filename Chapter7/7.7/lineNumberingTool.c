b3b07912e596b1c673da652687fe54865239661b
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-24 21:08:32 -0400

    Sprint 2: File Pointers and Printing

--- Chapter7/7.7/lineNumberingTool.c
+++ Chapter7/7.7/lineNumberingTool.c
@@ -7,18 +7,33 @@ Prints each line to stdout, prefixing it with a line number, like this:*/
 #include <stdio.h> 
 
 int main() {
+
     int c = 0;
-    FILE *input = ("input.txt", "r");
+    char linebuffer[256];
+    int line = 1;
+
+
+    FILE *input = fopen("input.txt", "r");
     if(input == NULL) {
         perror("couldn't open file!\n");
         return 1;
     }
-    FILE *output = ("output.txt", "w");
+
+    FILE *output = fopen("output.txt", "w");
     if(output == NULL) {
         perror("couldn't open file!\n");
         fclose(input);
         return 1;
     }
+
+    while(fgets(linebuffer, sizeof(linebuffer), input) != NULL) {
+        printf("%d: %s", line, linebuffer);
+        fprintf(output, ("%d: %s", line, linebuffer));
+        line++;
+    }
+
+    fclose(input);
+    fclose(output);
 
     return 0;
 }
