c6e779156861c6cae64ac54f49b80675d6dad036
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-24 20:06:49 -0400

    Sprint 1: Skeleton

---
+++ Chapter7/7.5/fileaccess.c
@@ -0,0 +1,19 @@
+/*
+Write a program that:
+Opens a file for reading (e.g., "input.txt")
+Opens another file for writing (e.g., "output.txt")
+Copies the contents of the first to the second, character by character
+Closes both files
+
+*/
+
+//time start 8:06
+//time stop 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    return 0;
+}
